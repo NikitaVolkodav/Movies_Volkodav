6ec30f13499ac68f57925db73edad9852937a29c
Nikita Volkodav <nikitavolkodav@MacBook-Air-Nikita.local>
2024-12-24 09:53:39 +0200

    added preloader module

---
+++ Movies_Volkodav/CustomViews/LoaderView.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+final class LoaderView: UIImageView {
+
+    func startRotating(duration: Double = 3.0,
+                       imageName: String = "circle.dashed",
+                       color: UIColor = .black) {
+        let systemImage = UIImage(systemName: imageName)?
+            .withRenderingMode(.alwaysOriginal)
+            .withTintColor(color)
+        image = systemImage
+        let rotation = CABasicAnimation(keyPath: "transform.rotation")
+        rotation.fromValue = 0
+        rotation.toValue = Double.pi * 2
+        rotation.duration = duration
+        rotation.repeatCount = .infinity
+        
+        self.layer.add(rotation, forKey: "rotationAnimation")
+    }
+    
+    func stopRotating() {
+        self.layer.removeAnimation(forKey: "rotationAnimation")
+        self.isHidden = true
+    }
+}
