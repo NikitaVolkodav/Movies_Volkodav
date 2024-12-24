5e3611e94c84fae541bf09ddfc8577f8119f5549
Nikita Volkodav <nikitavolkodav@MacBook-Air-Nikita.local>
2024-12-24 09:27:02 +0200

    add project

---
+++ Movies_Volkodav/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies_Volkodav
+//
+//  Created by Nikita Volkodav on 24.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
